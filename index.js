2ac97aa17d1b3748ea087faa66396b185b9f32bc
pipe473 <bedoyafelipe743@gmail.com>
2022-02-13 11:37:21 +0100

    Configuration to authenticated users in app

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: ({req}) => {
-        // console.log(req.headers['authorization']);        
+        // console.log(req.headers['authorization']);      
+        
+        // console.log(req.headers);
+        
+
         const token = req.headers['authorization'] || '';
         if(token){
             try {
                 const usuario = jwt.verify(token, process.env.SECRETA );
-
-                // console.log(usuario);
-
+                console.log(usuario);
                 return {
                     usuario
                 }
